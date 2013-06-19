36f614915b2cb2c2e4421494964558911948fcf6
Seecr Development Team <development@seecr.nl>
2013-06-19 09:25:22 +0200

    HM/TJ: license.

--- setup.py
+++ setup.py
@@ -2,8 +2,7 @@
 # -*- coding: utf-8 -*-
 ## begin license ##
 #
-# "Meresco Components" are components to build searchengines, repositories
-# and archives, based on "Meresco Core".
+# "Meresco Lucene" is a set of components and tools to integrate Lucene (based on PyLucene) into Meresco
 #
 # Copyright (C) 2007-2008 SURF Foundation. http://www.surf.nl
 # Copyright (C) 2007-2010 Seek You Too (CQ2) http://www.cq2.nl
@@ -11,22 +10,22 @@
 # Copyright (C) 2009-2010 Delft University of Technology http://www.tudelft.nl
 # Copyright (C) 2009 Tilburg University http://www.uvt.nl
 # Copyright (C) 2012-2013 Seecr (Seek You Too B.V.) http://seecr.nl
-# Copyright (C) 2012 Stichting Bibliotheek.nl (BNL) http://stichting.bibliotheek.nl
+# Copyright (C) 2012-2013 Stichting Bibliotheek.nl (BNL) http://www.bibliotheek.nl
 #
-# This file is part of "Meresco Components"
+# This file is part of "Meresco Lucene"
 #
-# "Meresco Components" is free software; you can redistribute it and/or modify
+# "Meresco Lucene" is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
 #
-# "Meresco Components" is distributed in the hope that it will be useful,
+# "Meresco Lucene" is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with "Meresco Components"; if not, write to the Free Software
+# along with "Meresco Lucene"; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #
 ## end license ##

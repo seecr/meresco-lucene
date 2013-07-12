a7146bb66ed6b60ceff52fbbdfe4f1978966ce72
Seecr Development Team <development@seecr.nl>
2013-07-12 09:41:39 +0200

    HM/TJ: build with jcc.

--- src/build.sh
+++ src/build.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
+mydir=$(cd $(dirname $0); pwd)
+buildDir=$mydir/build
+libDir=$mydir/lib
+
+rm -rf $buildDir
+mkdir $buildDir
+test -d $libDir || mkdir -p $libDir
 
 luceneJarDir=/usr/lib64/python2.6/site-packages/lucene
+classpath=${luceneJarDir}/lucene-core-4.3.0.jar:${luceneJarDir}/lucene-analyzers-common-4.3.0.jar
 
-javac -cp ${luceneJarDir}/lucene-core-4.3.0.jar:${luceneJarDir}/lucene-analyzers-common-4.3.0.jar org/meresco/lucene/MerescoStandardAnalyzer.java 
+javac -cp ${classpath} -d ${buildDir} org/meresco/lucene/MerescoStandardAnalyzer.java 
+(cd $buildDir; jar -c org > $libDir/meresco-lucene.jar)
+rm -rf $buildDir
+
+python -m jcc.__main__ \
+    --root $mydir/root \
+    --use_full_names \
+    --import lucene \
+    --shared \
+    --arch x86_64 \
+    --jar $libDir/meresco-lucene.jar \
+    --python meresco_lucene \
+    --build \
+    --install

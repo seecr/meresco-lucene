9fa186f6267a0905a93712f27fb42a6bf39798f7
Hendrik Moree <hendrik@seecr.nl>
2014-08-15 12:26:16 +0200

    HM: Lower cased java vars

--- src/org/meresco/lucene/search/SuperIndexSearcher.java
+++ src/org/meresco/lucene/search/SuperIndexSearcher.java
@@ -1,3 +1,28 @@
+/* begin license *
+ *
+ * "Meresco Lucene" is a set of components and tools to integrate Lucene (based on PyLucene) into Meresco
+ *
+ * Copyright (C) 2014 Seecr (Seek You Too B.V.) http://seecr.nl
+ * Copyright (C) 2014 Stichting Bibliotheek.nl (BNL) http://www.bibliotheek.nl
+ *
+ * This file is part of "Meresco Lucene"
+ *
+ * "Meresco Lucene" is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * "Meresco Lucene" is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with "Meresco Lucene"; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * end license */
+
 package org.meresco.lucene.search;
 
 import java.io.IOException;
@@ -23,15 +48,15 @@ public class SuperIndexSearcher extends IndexSearcher {
 		this.executor = executor;
 	}
 
-	public void search(Query Q, Filter F, SuperCollector C) throws IOException, InterruptedException,
+	public void search(Query q, Filter f, SuperCollector c) throws IOException, InterruptedException,
 			ExecutionException {
-		Weight weight = super.createNormalizedWeight(wrapFilter(Q, F));
+		Weight weight = super.createNormalizedWeight(wrapFilter(q, f));
 		ExecutorCompletionService<String> ecs = new ExecutorCompletionService<String>(this.executor);
 		// IDEA 1: group small leaves and sumbit those groups to avoid overhead
 		// IDEA 2: submit large leaves to pool, do small leaves her in main
 		// thread
 		for (AtomicReaderContext ctx : super.leafContexts) {
-			ecs.submit(new SearchTask(ctx, weight, C.subCollector(ctx)), "Done");
+			ecs.submit(new SearchTask(ctx, weight, c.subCollector(ctx)), "Done");
 		}
 		for (AtomicReaderContext _ : super.leafContexts) {
 			ecs.take().get();

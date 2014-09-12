2fef60e1e470dc6243c172b0c955b8d38093c7e9
Erik Groeneveld <erik@seecr.nl>
2014-09-12 16:36:27 +0200

    EG/HM: new better leaves grouping

--- src/org/meresco/lucene/search/SuperIndexSearcher.java
+++ src/org/meresco/lucene/search/SuperIndexSearcher.java
@@ -45,45 +45,60 @@ import org.apache.lucene.search.Weight;
 public class SuperIndexSearcher extends IndexSearcher {
 
     private ExecutorService executor;
-    private List<AtomicReaderContext> bigSegments;
-    private List<AtomicReaderContext> smallSegments;
+    private List<List<AtomicReaderContext>> grouped_leaves;
 
     public SuperIndexSearcher(DirectoryReader reader, ExecutorService executor) {
         super(reader);
         this.executor = executor;
-        findBigSegments();
-    }
+        this.grouped_leaves = this.group_leaves(reader.leaves());
+        for (List<AtomicReaderContext> l : this.grouped_leaves) {
+            int t = 0;
+            for (AtomicReaderContext ctx : l)
+                t += ctx.reader().numDocs();
+            System.out.print(" " + t + " ");
+        }
+        System.out.println();
+   }
 
-    private void findBigSegments() {
-        this.bigSegments = new ArrayList<AtomicReaderContext>(super.leafContexts);
-        Collections.sort(bigSegments, new Comparator<AtomicReaderContext>() {
+    protected List<List<AtomicReaderContext>> group_leaves(List<AtomicReaderContext> leaves) {
+        List<AtomicReaderContext> contexts = new ArrayList<AtomicReaderContext>(super.leafContexts);
+        Collections.sort(contexts, new Comparator<AtomicReaderContext>() {
             public int compare(AtomicReaderContext lhs, AtomicReaderContext rhs) {
-                return Integer.compare(lhs.reader().maxDoc(), rhs.reader().maxDoc());
+                return - Integer.compare(lhs.reader().maxDoc(), rhs.reader().maxDoc());
             }
         });
-        int totalSmallDocs = 0;
-        this.smallSegments = new ArrayList<AtomicReaderContext>();
-        if (this.bigSegments.isEmpty())
-            return;
-        int largestSegment = this.bigSegments.get(this.bigSegments.size() - 1).reader().maxDoc();
-        while (totalSmallDocs + this.bigSegments.get(0).reader().maxDoc() <= largestSegment) {
-            AtomicReaderContext context = this.bigSegments.remove(0);
-            totalSmallDocs += context.reader().maxDoc();
-            this.smallSegments.add(context);
-            if (this.bigSegments.isEmpty())
-                return;
+        List<List<AtomicReaderContext>> slices = new ArrayList<List<AtomicReaderContext>>(15);
+        for (int i = 0; i < 15; i++)
+            slices.add(new ArrayList<AtomicReaderContext>());
+        int avg_docs_per_slice = this.getTopReaderContext().reader().numDocs() / 15;
+        int biggest_segment_docs = contexts.get(0).reader().numDocs();
+        System.out.println(" biggest  segment: " + biggest_segment_docs);
+        long max_docs_per_slice = Math.max(avg_docs_per_slice, biggest_segment_docs);
+        System.out.println(" max doc per segment: " + max_docs_per_slice);
+        for (List<AtomicReaderContext> slice : slices) {
+            int docs_in_slice = 0;
+            while (docs_in_slice + contexts.get(0).reader().numDocs() <= max_docs_per_slice) {
+                AtomicReaderContext context = contexts.remove(0);
+                slice.add(context);
+                docs_in_slice += context.reader().numDocs();
+                if (contexts.isEmpty())
+                    return slices;
+            }
         }
+        int n = 0;
+        for (AtomicReaderContext context : contexts) {
+            slices.get(n++ % 15).add(context);
+        }
+        return slices;
     }
 
     public void search(Query q, Filter f, SuperCollector<?> c) throws IOException, InterruptedException,
             ExecutionException {
         Weight weight = super.createNormalizedWeight(wrapFilter(q, f));
         ExecutorCompletionService<String> ecs = new ExecutorCompletionService<String>(this.executor);
-        for (AtomicReaderContext ctx : this.bigSegments)
-            ecs.submit(new SearchTask(ctx, weight, c.subCollector()), "Done");
-        new SearchTask(this.smallSegments, weight, c.subCollector()).run();
-        System.currentTimeMillis();
-        for (int i = 0; i < this.bigSegments.size(); i++) {
+        for (List<AtomicReaderContext> leaf_group : this.grouped_leaves)
+            ecs.submit(new SearchTask(leaf_group, weight, c.subCollector()), "Done");
+        for (int i = 0; i < this.grouped_leaves.size(); i++) {
             ecs.take().get();
         }
     }

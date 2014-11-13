f26de6126cc7bed51f2c2395747ef3a871612dc5
Seecr Development Team <development@seecr.nl>
2014-11-13 16:54:23 +0100

    HM/JPM: further adaptation towards shared interface HasKeySetCache

--- src/org/meresco/lucene/search/join/HasKeySetCache.java
+++ src/org/meresco/lucene/search/join/HasKeySetCache.java
@@ -3,9 +3,12 @@ package org.meresco.lucene.search.join;
 import java.io.IOException;
 
 import org.apache.lucene.search.DocIdSet;
+import org.apache.lucene.search.Query;
 
 public interface HasKeySetCache {
     public DocIdSet getCollectedKeys() throws IOException;
+    
+    public Query getQuery();
 
 	public void printKeySetCacheSize();
 }

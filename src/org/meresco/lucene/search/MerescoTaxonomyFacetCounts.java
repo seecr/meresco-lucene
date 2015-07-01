1718eb7b9590c856313847907a2b1a60263691f9
Thijs Janssen <thijs@seecr.nl>
2015-07-01 11:54:31 +0200

    HM: Tabs to spaces

--- src/org/meresco/lucene/search/MerescoTaxonomyFacetCounts.java
+++ src/org/meresco/lucene/search/MerescoTaxonomyFacetCounts.java
@@ -39,43 +39,43 @@ import org.apache.lucene.util.IntsRef;
 
 public class MerescoTaxonomyFacetCounts extends IntTaxonomyFacets {
 
-	private final List<OrdinalsReader> ordinalsReaders;
-	private FacetsCollector fc;
+    private final List<OrdinalsReader> ordinalsReaders;
+    private FacetsCollector fc;
 
-	public MerescoTaxonomyFacetCounts(List<OrdinalsReader> ordinalsReaders, TaxonomyReader taxoReader, FacetsConfig config,
-			FacetsCollector fc, int[] valuesArray) throws IOException {
-		super(ordinalsReaders.get(0).getIndexFieldName(), taxoReader, config, valuesArray);
-		this.ordinalsReaders = ordinalsReaders;
-		this.fc = fc;
-	}
+    public MerescoTaxonomyFacetCounts(List<OrdinalsReader> ordinalsReaders, TaxonomyReader taxoReader, FacetsConfig config,
+            FacetsCollector fc, int[] valuesArray) throws IOException {
+        super(ordinalsReaders.get(0).getIndexFieldName(), taxoReader, config, valuesArray);
+        this.ordinalsReaders = ordinalsReaders;
+        this.fc = fc;
+    }
 
-	final void doCount() throws IOException {
-		count(this.fc.getMatchingDocs());
-	}
+    final void doCount() throws IOException {
+        count(this.fc.getMatchingDocs());
+    }
 
-	private final void count(List<MatchingDocs> matchingDocs) throws IOException {
-		IntsRef scratch = new IntsRef();
-		OrdinalsReader.OrdinalsSegmentReader[] ordsReaders = new OrdinalsReader.OrdinalsSegmentReader[this.ordinalsReaders.size()];
-		for (MatchingDocs hits : matchingDocs) {
-			for (int i = 0; i < ordsReaders.length; i++) {
-				ordsReaders[i] = this.ordinalsReaders.get(i).getReader(hits.context);
-			}
-			DocIdSetIterator docs = hits.bits.iterator();
-			int doc;
-			while ((doc = docs.nextDoc()) != DocIdSetIterator.NO_MORE_DOCS) {
-				for (OrdinalsReader.OrdinalsSegmentReader ords : ordsReaders) {
-					ords.get(doc, scratch);
-					for (int i = 0; i < scratch.length; i++) {
-						values[scratch.ints[scratch.offset + i]]++;
-					}
-				}
-			}
-		}
+    private final void count(List<MatchingDocs> matchingDocs) throws IOException {
+        IntsRef scratch = new IntsRef();
+        OrdinalsReader.OrdinalsSegmentReader[] ordsReaders = new OrdinalsReader.OrdinalsSegmentReader[this.ordinalsReaders.size()];
+        for (MatchingDocs hits : matchingDocs) {
+            for (int i = 0; i < ordsReaders.length; i++) {
+                ordsReaders[i] = this.ordinalsReaders.get(i).getReader(hits.context);
+            }
+            DocIdSetIterator docs = hits.bits.iterator();
+            int doc;
+            while ((doc = docs.nextDoc()) != DocIdSetIterator.NO_MORE_DOCS) {
+                for (OrdinalsReader.OrdinalsSegmentReader ords : ordsReaders) {
+                    ords.get(doc, scratch);
+                    for (int i = 0; i < scratch.length; i++) {
+                        values[scratch.ints[scratch.offset + i]]++;
+                    }
+                }
+            }
+        }
 
-		rollup();
-	}
+        rollup();
+    }
 
-	protected FacetsConfig.DimConfig verifyDim(String dim) {
-		return this.config.getDimConfig(dim);
-	}
+    protected FacetsConfig.DimConfig verifyDim(String dim) {
+        return this.config.getDimConfig(dim);
+    }
 }

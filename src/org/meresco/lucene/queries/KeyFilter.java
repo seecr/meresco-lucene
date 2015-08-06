d9ef81de7e3584a87febcc913651f254166325bd
Seecr Development Team <development@seecr.nl>
2015-08-06 11:20:29 +0200

    HM: Alway return NO_MORE_DOCS if keyValuesArray is null

--- src/org/meresco/lucene/queries/KeyFilter.java
+++ src/org/meresco/lucene/queries/KeyFilter.java
@@ -2,8 +2,9 @@
  *
  * "Meresco Lucene" is a set of components and tools to integrate Lucene (based on PyLucene) into Meresco
  *
- * Copyright (C) 2013-2014 Seecr (Seek You Too B.V.) http://seecr.nl
+ * Copyright (C) 2013-2015 Seecr (Seek You Too B.V.) http://seecr.nl
  * Copyright (C) 2013-2014 Stichting Bibliotheek.nl (BNL) http://www.bibliotheek.nl
+ * Copyright (C) 2015 Koninklijke Bibliotheek (KB) http://www.kb.nl
  *
  * This file is part of "Meresco Lucene"
  *
@@ -54,8 +55,7 @@ public class KeyFilter extends Filter {
 				return new DocIdSetIterator() {
 					private int[] keyValuesArray = KeyValuesCache.get(context, keyName);
 					private int maxDoc = context.reader().maxDoc();
-					int docId = keyValuesArray == null ? DocIdSetIterator.NO_MORE_DOCS
-							: 0;
+					int docId;
 
 					@Override
 					public int docID() {
@@ -64,15 +64,17 @@ public class KeyFilter extends Filter {
 
 					@Override
 					public int nextDoc() throws IOException {
-						try {
-							while (this.docId < this.maxDoc) {
-								int key = this.keyValuesArray[this.docId];
-								if (keySet.get(key)) {
-									return this.docId++;
+						if (keyValuesArray != null) {
+							try {
+								while (this.docId < this.maxDoc) {
+									int key = this.keyValuesArray[this.docId];
+									if (keySet.get(key)) {
+										return this.docId++;
+									}
+									docId++;
 								}
-								docId++;
+							} catch (IndexOutOfBoundsException e) {
 							}
-						} catch (IndexOutOfBoundsException e) {
 						}
 						this.docId = DocIdSetIterator.NO_MORE_DOCS;
 						return this.docId;

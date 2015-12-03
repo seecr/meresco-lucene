2238ba5e653e176fb9c6296ddce2e7d22f6cd3d8
Hendrik Morée <hendrik@seecr.nl>
2015-12-03 09:56:45 +0100

    HM: Added license

--- src/org/meresco/lucene/search/MerescoCluster.java
+++ src/org/meresco/lucene/search/MerescoCluster.java
@@ -1,3 +1,28 @@
+/* begin license *
+ *
+ * "Meresco Lucene" is a set of components and tools to integrate Lucene (based on PyLucene) into Meresco
+ *
+ * Copyright (C) 2015 Koninklijke Bibliotheek (KB) http://www.kb.nl
+ * Copyright (C) 2015 Seecr (Seek You Too B.V.) http://seecr.nl
+ *
+ * This file is part of "Meresco Lucene"
+ *
+ * "Meresco Lucene" is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * "Meresco Lucene" is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with "Meresco Lucene"; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * end license */
+
 package org.meresco.lucene.search;
 
 public final class MerescoCluster {
@@ -14,7 +39,7 @@ public final class MerescoCluster {
         public final int docId;
         public final double score;
         public String identifier;
-        
+
         public DocScore(int docId, double score) {
             this.docId = docId;
             this.score = score;

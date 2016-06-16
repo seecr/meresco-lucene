7a1bbd6aae4dafd91836b7778352eb23e014c54e
Jurjan-Paul Medema <jurjanpaul@seecr.nl>
2016-06-16 14:16:08 +0200

    JPM: adapted settings JSON format for cluster strategies

--- test/_integration/luceneservertest.py
+++ test/_integration/luceneservertest.py
@@ -108,8 +108,10 @@ class LuceneServerTest(IntegrationTestCase):
                 'similarity': 'BM25(k1=1.2,b=0.75)',
                 'clustering': {
                     'clusterMoreRecords': 100,
-                    'clusteringEps': 0.4,
-                    'clusteringMinPoints': 1,
+                    'strategies': [{
+                        'clusteringEps': 0.4,
+                        'clusteringMinPoints': 1,
+                    }]
                 },
                 'commitCount': 1,
                 'commitTimeout': 10,

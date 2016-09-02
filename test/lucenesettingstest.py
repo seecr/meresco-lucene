c52fd412408b5052d226b635c41bdefa50224cd1
Seecr Development Team <development@seecr.nl>
2016-09-02 11:08:28 +0200

    EG: re-added tests mergePolicy + refactorings

--- test/lucenesettingstest.py
+++ test/lucenesettingstest.py
@@ -25,48 +25,64 @@
 ## end license ##
 
 from seecr.test import SeecrTestCase
+from copy import copy
 from meresco.lucene import LuceneSettings
 from meresco.lucene.fieldregistry import FieldRegistry
+
+DEFAULTS = dict(
+    lruTaxonomyWriterCacheSize = 4000,
+    mergePolicy = dict(type = "TieredMergePolicy", segmentsPerTier=8.0, maxMergeAtOnce=2),
+    similarity = {'type': 'BM25Similarity'},
+    numberOfConcurrentTasks = 6,
+    analyzer = {'type': 'MerescoStandardAnalyzer'},
+    drilldownFields = [],
+    commitCount = 100000,
+    commitTimeout = 10,
+    cacheFacetOrdinals = True,
+    verbose = True,
+    readonly = False
+)
 
 class LuceneSettingsTest(SeecrTestCase):
 
     def testOne(self):
         settings = LuceneSettings()
         self.assertTrue(settings.verbose)
-
         newSettings = settings.clone(verbose=False)
         self.assertTrue(settings.verbose)
         self.assertFalse(newSettings.verbose)
 
     def testAsPostDict(self):
         settings = LuceneSettings()
-        self.assertEqual({
-                'lruTaxonomyWriterCacheSize': 4000,
-                'maxMergeAtOnce': 2,
-                'similarity': {'type': 'BM25Similarity'},
-                'numberOfConcurrentTasks': 6,
-                'segmentsPerTier': 8.0,
-                'analyzer': {'type': 'MerescoStandardAnalyzer'},
-                'drilldownFields': [],
-                'commitCount': 100000,
-                'commitTimeout': 10
-            }, settings.asPostDict())
+        self.assertEquals(DEFAULTS.keys(), settings.asPostDict().keys())
+        self.assertEquals(DEFAULTS, settings.asPostDict())
 
     def testPostDictWithDrilldownFields(self):
         fieldRegistry = FieldRegistry()
         fieldRegistry.registerDrilldownField("field0", hierarchical=True, multiValued=False)
         fieldRegistry.registerDrilldownField("field1", hierarchical=True, multiValued=True, indexFieldName="$facets_2")
         settings = LuceneSettings(fieldRegistry=fieldRegistry)
-        self.assertEqual({
-                'lruTaxonomyWriterCacheSize': 4000,
-                'maxMergeAtOnce': 2,
-                'similarity': {'type': 'BM25Similarity'},
-                'numberOfConcurrentTasks': 6,
-                'segmentsPerTier': 8.0,
-                'analyzer': {'type': 'MerescoStandardAnalyzer'},
-                'drilldownFields': [
-                    {'dim': 'field0', 'hierarchical': True, 'fieldname': None, 'multiValued': False},
-                    {'dim': 'field1', 'hierarchical': True, 'fieldname': '$facets_2', 'multiValued': True}],
-                'commitCount': 100000,
-                'commitTimeout': 10
-            }, settings.asPostDict())
+        soll = copy(DEFAULTS)
+        soll['drilldownFields'] = [
+            {'dim': 'field0', 'hierarchical': True, 'fieldname': None, 'multiValued': False},
+            {'dim': 'field1', 'hierarchical': True, 'fieldname': '$facets_2', 'multiValued': True}]
+        self.assertEquals(soll, settings.asPostDict())
+
+    def testConfigureOrdinalsCache(self):
+        settings = LuceneSettings(cacheFacetOrdinals=False)
+        soll = copy(DEFAULTS)
+        soll['cacheFacetOrdinals'] = False
+        ist = settings.asPostDict()
+        self.assertEquals(soll.keys(), ist.keys())
+        self.assertEquals(soll, ist)
+
+    def testConfigureMergePolicy(self):
+        settings = LuceneSettings(mergePolicy={'type':'LogDocMergePolicy',
+            'mergeFactor': 2,
+            'maxMergeDocs': 100})
+        soll = copy(DEFAULTS)
+        soll['mergePolicy'] = dict(type='LogDocMergePolicy', mergeFactor=2, maxMergeDocs=100)
+        ist = settings.asPostDict()
+        self.assertEquals(soll.keys(), ist.keys())
+        self.assertEquals(soll, ist)
+

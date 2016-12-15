5245d6c9baf42555c6e67fa858de5ed61d242a57
Jurjan-Paul Medema <jurjanpaul@seecr.nl>
2016-12-15 15:35:58 +0100

    JPM: fixed issue that special character containing long facet value was cut off in between utf-8 character bytes

--- meresco/lucene/fieldslisttolucenedocument.py
+++ meresco/lucene/fieldslisttolucenedocument.py
@@ -1,28 +1,25 @@
 ## begin license ##
 #
-# "NBC+" also known as "ZP (ZoekPlatform)" is
-#  a project of the Koninklijke Bibliotheek
-#  and provides a search service for all public
-#  libraries in the Netherlands.
+# "Meresco Lucene" is a set of components and tools to integrate Lucene (based on PyLucene) into Meresco
 #
-# Copyright (C) 2014-2015 Seecr (Seek You Too B.V.) http://seecr.nl
+# Copyright (C) 2014-2016 Seecr (Seek You Too B.V.) http://seecr.nl
 # Copyright (C) 2014 Stichting Bibliotheek.nl (BNL) http://www.bibliotheek.nl
 # Copyright (C) 2015 Koninklijke Bibliotheek (KB) http://www.kb.nl
 #
-# This file is part of "NBC+ (Zoekplatform BNL)"
+# This file is part of "Meresco Lucene"
 #
-# "NBC+ (Zoekplatform BNL)" is free software; you can redistribute it and/or modify
+# "Meresco Lucene" is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
 #
-# "NBC+ (Zoekplatform BNL)" is distributed in the hope that it will be useful,
+# "Meresco Lucene" is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with "NBC+ (Zoekplatform BNL)"; if not, write to the Free Software
+# along with "Meresco Lucene"; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #
 ## end license ##
@@ -51,7 +48,7 @@ class FieldsListToLuceneDocument(Observable):
         if self._fieldRegistry.isDrilldownField(fieldname):
             lvalue = value
             if isinstance(lvalue, basestring):
-                lvalue = [str(lvalue)]
+                lvalue = [unicode(lvalue)]
             lvalue = [v[:MAX_STRING_LENGTH] for v in lvalue]
             fields.append(self._fieldRegistry.createFacetField(fieldname, lvalue))
         if self._fieldRegistry.isIndexField(fieldname):

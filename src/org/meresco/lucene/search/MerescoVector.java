1e59ca95cf153128ff3febc9bd002d5d3f62b417
erik <erik@192.168.208.1>
2019-10-31 13:32:12 +0100

    EG: wip

--- src/org/meresco/lucene/search/MerescoVector.java
+++ src/org/meresco/lucene/search/MerescoVector.java
@@ -32,7 +32,7 @@ import org.apache.commons.math3.util.OpenIntToDoubleHashMap.Iterator;
 import org.apache.lucene.util.BytesRef;
 import org.apache.lucene.util.BytesRefHash;
 
-class MerescoVector implements Clusterable {
+public class MerescoVector implements Clusterable {
     private OpenIntToDoubleHashMap entries;
     int docId;
     private int maxIndex;
@@ -92,4 +92,11 @@ class MerescoVector implements Clusterable {
         }
         System.out.println();
     }
+
+	@Override
+	public String toString() {
+		return "MerescoVector [docId=" + docId + "]";
+	}
+    
+    
 }

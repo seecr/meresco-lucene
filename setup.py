1bf115bb2fbfd63f47ad1f385df5ddaadde54983
Thijs Janssen <thijs@seecr.nl>
2021-02-03 12:23:47 +0100

    TJ: start of meresco-lucene.

--- setup.py
+++ setup.py
@@ -1,17 +1,21 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 ## begin license ##
 #
-# "Meresco Lucene" is a set of components and tools to integrate Lucene (based on PyLucene) into Meresco
+# "Meresco Lucene" is a set of components and tools to integrate Lucene into Meresco
 #
 # Copyright (C) 2007-2008 SURF Foundation. http://www.surf.nl
 # Copyright (C) 2007-2010 Seek You Too (CQ2) http://www.cq2.nl
 # Copyright (C) 2007-2009 Stichting Kennisnet Ict op school. http://www.kennisnetictopschool.nl
 # Copyright (C) 2009-2010 Delft University of Technology http://www.tudelft.nl
 # Copyright (C) 2009 Tilburg University http://www.uvt.nl
-# Copyright (C) 2012-2013, 2015, 2019 Seecr (Seek You Too B.V.) https://seecr.nl
+# Copyright (C) 2012-2013, 2015, 2019, 2021 Seecr (Seek You Too B.V.) https://seecr.nl
 # Copyright (C) 2012-2013 Stichting Bibliotheek.nl (BNL) http://www.bibliotheek.nl
 # Copyright (C) 2015 Koninklijke Bibliotheek (KB) http://www.kb.nl
+# Copyright (C) 2021 Data Archiving and Network Services https://dans.knaw.nl
+# Copyright (C) 2021 SURF https://www.surf.nl
+# Copyright (C) 2021 Stichting Kennisnet https://www.kennisnet.nl
+# Copyright (C) 2021 The Netherlands Institute for Sound and Vision https://beeldengeluid.nl
 #
 # This file is part of "Meresco Lucene"
 #
@@ -69,7 +73,7 @@ setup(
         ]
     },
     version = version,
-    url = 'http://seecr.nl',
+    url = 'https://seecr.nl',
     author = 'Seecr (Seek You Too B.V.)',
     author_email = 'info@seecr.nl',
     description = 'A set of components and tools to integrate Lucene into Meresco',
--- testsetup.sh
+++ testsetup.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 ## begin license ##
 #
-# "Meresco Lucene" is a set of components and tools to integrate Lucene (based on PyLucene) into Meresco
+# "Meresco Lucene" is a set of components and tools to integrate Lucene into Meresco
 #
-# Copyright (C) 2012-2013, 2015-2016, 2019 Seecr (Seek You Too B.V.) https://seecr.nl
+# Copyright (C) 2012-2013, 2015-2016, 2019, 2021 Seecr (Seek You Too B.V.) https://seecr.nl
 # Copyright (C) 2013 Stichting Bibliotheek.nl (BNL) http://www.bibliotheek.nl
 # Copyright (C) 2015 Koninklijke Bibliotheek (KB) http://www.kb.nl
+# Copyright (C) 2021 Data Archiving and Network Services https://dans.knaw.nl
+# Copyright (C) 2021 SURF https://www.surf.nl
+# Copyright (C) 2021 Stichting Kennisnet https://www.kennisnet.nl
+# Copyright (C) 2021 The Netherlands Institute for Sound and Vision https://beeldengeluid.nl
 #
 # This file is part of "Meresco Lucene"
 #
@@ -30,11 +34,9 @@ rm -rf tmp build
 mydir=$(cd $(dirname $0); pwd)
 source /usr/share/seecr-tools/functions.d/test
 
-pyversion="python2.7"
 VERSION="x.y.z"
 
-definePythonVars $pyversion
-echo "###### $pyversion, $PYTHON"
+definePythonVars
 (cd $mydir/src_pylucene; ./build.sh ${SITEPACKAGES}/meresco/lucene)
 (
     cd $mydir
@@ -46,6 +48,7 @@ ${PYTHON} setup.py install --root tmp
 
 cp -r test tmp/test
 removeDoNotDistribute tmp
+
 find tmp -name '*.py' -exec sed -r -e "
     s/\\\$Version:[^\\\$]*\\\$/\\\$Version: ${VERSION}\\\$/;
     sX^usrSharePath.*=.*XusrSharePath = '${mydir}/tmp/usr/share/meresco-lucene'X;
